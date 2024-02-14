6c6cacb2cbea6104cd82f6806c3576b40455c6e9
prabhtumber <ptumber@uw.edu>
2024-02-13 20:09:06 -0800

    Added Base Room class with some logic.

---
+++ TriviaMazeRoom.java
@@ -0,0 +1,33 @@
+public class TriviaMazeRoom extends TriviaMazeDoor {
+    private TriviaMazeRoom[] myDoors;
+    private Question_Answer myQuestion;
+
+    public TriviaMazeRoom(int numberOfDoors) {
+        myDoors = new TriviaMazeRoom[numberOfDoors];
+        for (int i = 1; i < numberOfDoors; i++) {
+
+        }
+        myQuestion = new Question_Answer();
+    }
+
+    public boolean unlockDoor(int index) {
+        if (index >= 0 && index < myDoors.length && !myDoors[index].isLocked()) {
+            myDoors[index].unlock();
+            return true;
+        }
+        return false;
+    }
+
+    public Question getQuestion() {
+        return myQuestion.getQuestion();
+    }
+
+    public class Question {
+    }
+
+    public class Question_Answer {
+        public Question getQuestion() {
+            return new Question();
+        }
+    }
+}

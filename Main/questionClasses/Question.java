0737d52dd709fd3d44f8e74e96d0ea79054a961c
prabhtumber <ptumber@uw.edu>
2024-02-22 00:21:46 -0800

    Added the database to the repo.

---
+++ Main/questionClasses/Question.java
@@ -0,0 +1,27 @@
+
+public class Question {
+
+    private final String questionText;
+    private final String answerText;
+
+    public Question(final String question, final String answer) {
+        questionText = question;
+        answerText = answer;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String getCorrectAnswer() {
+        return answerText;
+    }
+
+    public String getQuestion() {
+        return questionText;
+    }
+
+    public String getAnswer() {
+        return answerText;
+    }
+}

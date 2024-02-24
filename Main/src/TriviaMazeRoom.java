bd0217bdfbe74f74bc1dcfd5a19d9502ff1230bf
prabhtumber <ptumber@uw.edu>
2024-02-24 00:03:48 -0800

    Altered Room class and started working on the Main class.

--- Main/src/TriviaMazeRoom.java
+++ Main/src/TriviaMazeRoom.java
@@ -1,33 +1,23 @@
+import questionClasses.*;
+
 public class TriviaMazeRoom extends TriviaMazeDoor {
     private TriviaMazeRoom[] myDoors;
-    private Question_Answer myQuestion;
+    private Question myQuestion;
 
     public TriviaMazeRoom(int numberOfDoors) {
         myDoors = new TriviaMazeRoom[numberOfDoors];
         for (int i = 1; i < numberOfDoors; i++) {
 
         }
-        myQuestion = new Question_Answer();
+
     }
 
     public boolean unlockDoor(int index) {
         if (index >= 0 && index < myDoors.length && !myDoors[index].isMyDoorLocked()) {
-            myDoors[index].unlock();
+            myDoors[index].isMyDoorLocked();
             return true;
         }
         return false;
     }
 
-    public Question getQuestion() {
-        return myQuestion.getQuestion();
-    }
-
-    public class Question {
-    }
-
-    public class Question_Answer {
-        public Question getQuestion() {
-            return new Question();
-        }
-    }
 }

c4516435980ed71922b1a0ab21972e57afc77359
prabhtumber <ptumber@uw.edu>
2024-02-28 23:21:10 -0800

    made changes to the room class to use ordinal directions.

--- Main/src/TriviaMazeRoom.java
+++ Main/src/TriviaMazeRoom.java
@@ -1,23 +1,60 @@
-import questionClasses.*;
-
-public class TriviaMazeRoom extends TriviaMazeDoor {
-    private TriviaMazeRoom[] myDoors;
-    private Question myQuestion;
-
-    public TriviaMazeRoom(int numberOfDoors) {
-        myDoors = new TriviaMazeRoom[numberOfDoors];
-        for (int i = 1; i < numberOfDoors; i++) {
-
-        }
-
+public class TriviaMazeRoom {
+    private enum Direction {
+        NORTH, WEST, SOUTH, EAST
     }
 
-    public boolean unlockDoor(int index) {
-        if (index >= 0 && index < myDoors.length && !myDoors[index].isMyDoorLocked()) {
-            myDoors[index].isMyDoorLocked();
-            return true;
-        }
-        return false;
+    private final TriviaMazeDoor[] myDoors = new TriviaMazeDoor[4];
+    private boolean myHasVisited;
+
+    public TriviaMazeRoom(TriviaMazeDoor directionNorth, TriviaMazeDoor directionWest,
+            TriviaMazeDoor directionSouth, TriviaMazeDoor directionEast) {
+        myHasVisited = false;
+        myDoors[Direction.NORTH.ordinal()] = directionNorth;
+        myDoors[Direction.WEST.ordinal()] = directionWest;
+        myDoors[Direction.SOUTH.ordinal()] = directionSouth;
+        myDoors[Direction.EAST.ordinal()] = directionEast;
     }
 
+    public TriviaMazeDoor getDoor(Direction direction) {
+        return myDoors[direction.ordinal()];
+    }
+
+    public boolean visited() {
+        return myHasVisited;
+    }
+
+    public void markVisited(boolean theVisit) {
+        myHasVisited = theVisit;
+    }
+
+    @Override
+    public String toString() {
+        String[] doorStates = new String[4];
+        for (int i = 0; i < myDoors.length; i++) {
+            if (myDoors[i] == null) {
+                doorStates[i] = "XX";
+            } else if (myDoors[i].isMyDoorLocked() && !myDoors[i].isMyDoorLockedPermanent()) {
+                doorStates[i] = "LK";
+            } else if (myDoors[i].isMyDoorLockedPermanent()) {
+                doorStates[i] = "XX";
+            } else {
+                doorStates[i] = "OP";
+            }
+        }
+        return String.format("""
+                             ROOM
+                ______________________________
+                              NORTH
+                             ____
+                             |%s|
+                        ____ ---- ____
+                WEST    |%s|  PL  |%s|    EAST
+                        ---- ____ ----
+                             |%s|
+                             ----
+                              SOUTH
+                ------------------------------
+                """, doorStates[Direction.NORTH.ordinal()], doorStates[Direction.WEST.ordinal()],
+                doorStates[Direction.EAST.ordinal()], doorStates[Direction.SOUTH.ordinal()]);
+    }
 }
